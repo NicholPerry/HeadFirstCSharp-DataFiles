00f3e8817f96fb785849ae9cb42561d793bff8dc
Nichol Perry <nichol.rae@gmail.com>
2023-02-18 11:54:30 -0600

    Initial commit

---
+++ OneDrive/Desktop/2021 Spring/Object Orientated Design/8.5 DataBinding Perry/DataBinding/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int boundNumber;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int BoundNumber
+        {
+            get
+            {
+                return this.boundNumber;
+            }
+            set
+            {
+                if (value != boundNumber)
+                {
+                    boundNumber = value;
+                    OnPropertyChanged("BoundNumber");
+                }
+            }
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
